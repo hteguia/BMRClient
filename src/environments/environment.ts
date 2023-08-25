b457c1ddad39e206a296cce1146e0631db7d1e8d
Hervé TEGUIA <tkh.tegus@gmail.com>
2023-08-25 09:37:48 +0400

    env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,5 +3,5 @@ export const environment = {
     apiUrl: 'http://localhost:3000/api',
     version: '3.0.0',
     appName: 'BMR-AFRICA',
-    googleClientId: '964990975366-egjl82f0vo2e7tiaflvdfjqefuf4m3rc.apps.googleusercontent.com',
+    googleClientId: 'xxxxxxxxxxxxxxxxxxxxxxxxxxxx',
   };
