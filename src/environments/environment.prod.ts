d063c4c77e1968fb3dc612bf06bb59781e397a8c
Hervé TEGUIA <tkh.tegus@gmail.com>
2023-11-01 20:59:42 +0400

    up

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-    apiUrl: 'http://notification.teguia.me/api',
+    apiUrl: 'https://notification.teguia.me/api',
     version: '3.0.0',
     appName: 'BMR-AFRICA'
   };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
-    apiUrl: 'https://localhost:5001/api',
+    apiUrl: 'https://notification.teguia.me/api',
     version: '3.0.0',
     appName: 'BMR-AFRICA',
     googleClientId: '',
