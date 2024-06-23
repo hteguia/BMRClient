7c7d62eb26a08e2811c27a1f1db2f3d188bb6cf8
Hervé TEGUIA <tkh.tegus@gmail.com>
2024-06-23 17:12:18 +0400

    cookie auth only

--- src/app/core/services/interceptors/auth.interceptor.ts
+++ src/app/core/services/interceptors/auth.interceptor.ts
@@ -7,14 +7,18 @@ import { AuthService } from "src/app/auth/services/auth.service";
 export class AuthInterceptor implements HttpInterceptor {
   private authService = inject(AuthService);
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    const authToken = this.authService.accessToken?.token;
-      if(authToken){
-        req = req.clone({
-          setHeaders: {
-            Authorization: `Bearer ${authToken}`
-          }
-        });
-      }
+    // const authToken = this.authService.accessToken?.token;
+    //   if(authToken){
+    //     req = req.clone({
+    //       setHeaders: {
+    //         Authorization: `Bearer ${authToken}`
+    //       }
+    //     });
+    //   }
+
+      req = req.clone({
+        withCredentials: true
+      });
       
       return next.handle(req);
   }
